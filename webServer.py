0d7dad1a70b38f28e0eb6fef126d9c906c1c8d20
aig2088-gummi <aig2088@nyu.edu>
2025-09-26 22:53:57 -0400

    Add files via upload

--- webServer.py
+++ webServer.py
@@ -1,83 +1,58 @@
-# import socket module
-from socket import *
-# In order to terminate the program
-import sys
-
-
-
-
-# Example of how to structure the response
-response_body = "<h1>Hello, World!</h1>"
-content_length = len(response_body.encode('utf-8'))
-
-response_headers = (
-    "HTTP/1.1 200 OK\r\n"
-    "Server: MyCustomPythonServer\r\n"  # Add the Server header
-    "Connection: close\r\n"              # Add the Connection header
-    "Content-Type: text/html\r\n"
-    f"Content-Length: {content_length}\r\n"
-    "\r\n"  # A blank line indicates the end of headers
-)
-
-full_response = response_headers.encode('utf-8') + response_body.encode('utf-8')
+from socket import *  # Import all socket functions
 
 
 def webServer(port=13331):
+    # Create a TCP socket (like a door for communication)
     serverSocket = socket(AF_INET, SOCK_STREAM)
 
-    # Prepare a server socket
+    # Bind the socket to the local host and the given port
     serverSocket.bind(("", port))
-    serverSocket.listen(1)  # Start listening for incoming connections
-    print("Ready to serve on port...", port)
+
+    # Start listening for incoming connections (1 at a time)
+    serverSocket.listen(1)
+    print("Server is running on port", port)
 
     while True:
-        # Establish the connection
-        print('Ready to serve...')
-        connectionSocket, addr = serverSocket.accept()  # Accept client connection
+        # Wait for a client (web browser) to connect
+        connectionSocket, addr = serverSocket.accept()
 
         try:
-            message = connectionSocket.recv(1024).decode()  # Receive client message
-            filename = message.split()[1]  # Extract the filename from the GET request
+            # Read the HTTP request message from the client
+            message = connectionSocket.recv(1024).decode()
 
-            # Open the client requested file (remove the leading slash)
-            f = open(filename[1:], 'rb')  # Open file in binary mode
+            # Extract the requested filename from the message
+            filename = message.split()[1]  # e.g., "/index.html"
 
-            # Send HTTP response header line for a successful request
-            header = "HTTP/1.1 200 OK\r\n"
-            Server: PythonWebServer / 1.0
-            Connection: close
-            contentType = "Content-Type: text/html; charset=UTF-8\r\n"
-            endOfHeader = "\r\n"
+            # Open the requested file (skip the first "/" in the filename)
+            f = open(filename[1:], 'r')
+            outputdata = f.read()
+            f.close()  # Close the file after reading
 
+            # --- Send HTTP 200 OK headers ---
+            connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())  # Status line
+            connectionSocket.send("Server: SimpleServer\r\n".encode())  # Server header
+            connectionSocket.send("Connection: close\r\n".encode())  # Close after response
+            connectionSocket.send("Content-Type: text/html\r\n".encode())  # File type
+            connectionSocket.send("\r\n".encode())  # Blank line signals end of headers
 
-            # Read the file content
-            fileContent = f.read()
+            # Send the content of the requested file
+            connectionSocket.send(outputdata.encode())
 
-            # Send the response header and file content
-            response = header + contentType + endOfHeader
-            connectionSocket.send(response.encode() + fileContent)
+        except:
+            # --- File not found: send HTTP 404 headers ---
+            connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())  # Status line
+            connectionSocket.send("Server: SimpleServer\r\n".encode())  # Server header
+            connectionSocket.send("Connection: close\r\n".encode())  # Close after response
+            connectionSocket.send("Content-Type: text/html\r\n".encode())  # File type
+            connectionSocket.send("\r\n".encode())  # End of headers
 
-            # Close the file
-            f.close()
+            # Send a simple HTML 404 page
+            connectionSocket.send("<h1>404 Not Found</h1>".encode())
 
-            # Close the client connection socket
-            connectionSocket.close()
-
-        except Exception as e:
-            # Send response message for file not found
-            header = "HTTP/1.1 404 Not Found\r\n"
-            contentType = "Content-Type: text/html; charset=UTF-8\r\n"
-            endOfHeader = "\r\n"
-            body = "<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n"
-
-
-            response = header + contentType + endOfHeader + body
-            connectionSocket.send(response.encode())
-
-            # Close the client connection socket
-            connectionSocket.close()
+        # Close the connection to the client
+        connectionSocket.close()
 
 
-# Entry point
+# Run the server if this file is executed
 if __name__ == "__main__":
     webServer(13331)
